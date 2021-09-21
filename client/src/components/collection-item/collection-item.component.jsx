4f4d39fd62d4cb466e9f573678b7291f5cbc7f93
Rodolfo Eliezer <rodolfo.eliezer@actiosoftware.com.br>
2021-09-20 21:33:57 -0300

    salvando google analitics

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -23,7 +23,7 @@ const CollectionItem = ({ item, addItem }) => {
         <PriceContainer>{price}</PriceContainer>
       </CollectionFooterContainer>
       <AddButton onClick={() => addItem(item)} inverted>
-        Add to cart
+        Adicionar no carrinho
       </AddButton>
     </CollectionItemContainer>
   );
